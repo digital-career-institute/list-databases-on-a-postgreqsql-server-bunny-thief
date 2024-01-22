d4de78861d4bc87ab92000c22b1f8f447171458a
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-01-22 16:22:09 +0100

    Add getProperties method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,44 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             var stm = conn.prepareStatement("SELECT datname FROM pg_database");) {
+
+            ResultSet rs = stm.executeQuery();
+
+            while (rs.next()) {
+                System.out.printf("%s\n", rs.getString(1));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
+
+    public static Properties getProperties() {
+        String path = String.format("src/main/resources/db.properties", System.getProperty("user.dir"));
+        Properties properties = new Properties();
+
+        try (InputStream is = new FileInputStream(new File(path))) {
+            properties.load(is);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+        return properties;
+    }
+
 }
